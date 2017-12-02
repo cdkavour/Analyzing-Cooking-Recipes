ae966b4bacf8a0d598c1308f0a8161ab71e5e3d7
ljlight <ljlight@umich.edu>
2017-12-02 15:09:26 -0500

    Updated scraper_get_recipes.py to get all recipes from first scroll

--- scraper_get_recipes.py
+++ scraper_get_recipes.py
@@ -4,33 +4,23 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-
-
 def main():
-	urls = open("Category_urls.txt").read().splitlines()
-
-	for base_url in urls:
-		i = 1
-		while True:
-			# Create soup for each category page
-			url = base_url + '?page=' + str(i) + '#' + str(i)
-			bytes = urlopen(url).read()
-			soup = BeautifulSoup(bytes, 'lxml')
-			for recipe in soup.find_all('article', 'fixed-recipe-card'):
-				print(recipe.find('a', 'ng-isolate-scope'), '\n\n')
-			# # Loop over the categories and snag the url they point to
-			# category_urls = []
-			# for category in soup.find_all('a', href=True, class_="hero-link__item"):
-			# 	href = category['href']
-			# 	if href[0] == '/':
-			# 		category_urls.append('allrecipes.com' + href + '\n')
-			# with open("Category_urls.txt", 'w') as output:
-			# 	for URL in category_urls:
-			# 		if "ingredients" in URL:
-			# 			continue
-			# 		output.write(URL)
-			break
-		break
+    urls = open("Category_urls.txt").read().splitlines()
+    output = open("Recipe_urls.txt", 'w')
+    recipe_urls = set()
+    for base_url in urls:
+        i = 1
+        print(base_url)
+        # Create soup for each category page
+        url = base_url + '?page=' + str(i) + '#' + str(i)
+        bytes = urlopen(url).read()
+        soup = BeautifulSoup(bytes, 'lxml')
+        for recipe in soup.find_all('article', 'fixed-recipe-card'):
+            x = recipe.find('a', 'fixed-recipe-card__title-link')
+            if x:
+                recipe_urls.add(x.get('href'))
+    for recipe in recipe_urls:
+        output.write('http://allrecipes.com' + recipe + '\n')
 
 if __name__ == '__main__':
-	main()
+    main()
